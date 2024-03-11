612fa7f5393bc36448bddd3cc9f79f2c20e8e21a
shahriar <shahriar.asraf.777@gmail.com>
2024-03-12 01:05:02 +0600

    mongoose database connection successfully

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,18 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-
+import dbConnection from "./DB/DbConnect.js";
 //configure dotenv
 dotenv.config({ path: "./.env" });
 
 //listen server
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`server is runing port ${port}`);
-});
+dbConnection()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is runing port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed", error);
+  });
