26e7f630887d486f0de416e638d45e9101aaa48c
Shahriar <Shahriar.asraf@yahoo.com>
2024-03-12 03:00:05 +0600

    add global error  Controller and customError class

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,39 @@
-import express from "express";
-
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import customError from "./utils/customError.js";
+import { globalError } from "./controllers/globalError.Controller.js";
 const app = express();
+
+//add middelwares
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ limit: "2000kb", extended: true }));
+app.use(express.static("public"));
+
+//Add Routes
+app.use("/api/v1/users", (req, res) => {
+  res.send("<h1>hello</h1>");
+});
+
+//Invalid  Routes Error handles
+app.all("*", (req, res, next) => {
+  const error = new customError(
+    `can't find ${req.originalUrl} on the Server`,
+    404
+  );
+  next(error);
+});
+
+//global error middelware
+app.use(globalError);
 
 export default app;
---
+++ Backend/src/controllers/globalError.Controller.js
@@ -0,0 +1,9 @@
+export const globalError = (error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || "error";
+
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+  });
+};
